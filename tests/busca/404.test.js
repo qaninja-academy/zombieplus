e9696166f72278fbfc559a4459d355c67e455e2d
Papito <eu@papito.io>
2019-10-26 14:56:55 -0300

    simulando um bug

--- tests/busca/404.test.js
+++ tests/busca/404.test.js
@@ -24,7 +24,7 @@ module.exports = {
 
         movie
             .waitForElementVisible('@alertDanger', 10000)
-            .assert.containsText('@alertDanger', 'Puxa! não encontramos nada aqui :(')
+            .assert.containsText('@alertDanger', 'Puxa! não encontramos nada aqui.')
     }
 
 
